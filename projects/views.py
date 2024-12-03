58290465014a654379575b0a4f33dadf4f35c88c
YiqingLiDev <yiqingli07@gmail.com>
2024-12-03 22:02:33 +0000

    add the table to page

--- projects/views.py
+++ projects/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 
 # Create your views here.
 
 def projects(request):
+
+    projects = Project.objects.all()
+
     template = 'projects/projects.html'
-    context = {}
+    context = {'projects': projects}
 
     return render(request, template, context)
 
 
 def project(request, pk):
+
+    projectobj = Project.objects.get(id=pk)
+
     template = 'projects/single_project.html'
-    context = {'pk': pk}
+    context = {'pk': pk, 'project': projectobj}
+
 
     return render(request, template, context)
