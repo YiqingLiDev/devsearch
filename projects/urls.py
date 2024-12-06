9dcbf955a5b8fecabadc743ca245d65846784b21
YiqingLiDev <yiqingli07@gmail.com>
2024-12-06 19:36:40 +0000

    add forms

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
+    path('create_project/', views.createProjects, name='createprojects'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
 
 # Create your views here.
@@ -22,5 +23,15 @@ def project(request, pk):
     template = 'projects/single_project.html'
     context = {'pk': pk, 'project': projectobj}
 
+
+    return render(request, template, context)
+
+
+def createProjects(request):
+
+    form = ProjectForm()
+
+    template = 'projects/project_form.html'
+    context = {'form': form}
 
     return render(request, template, context)
