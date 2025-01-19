0d156ea9d08538efc9a1faf407b99b728636a44a
YiqingLiDev <yiqingli07@gmail.com>
2025-01-19 21:28:41 +0000

    modify users profile page

--- users/views.py
+++ users/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 
 def profiles(request):
 
-    user = request.user
-    if user.is_authenticated:
-        test = user.get_user_permissions()
-    
-    print(f'the fullname is {test}')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
 
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+
+    return render(request, 'users/user-profile.html', context)
+
 
 
 
